7cbdd1471328f380900b6e7626b1a11723c6ed1e
Induce <aa5619721@gmail.com>
2026-03-22 22:16:33 +0300

    iter 1

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -3,50 +3,50 @@
 #ifndef INCLUDE_TIMEDDOOR_H_
 #define INCLUDE_TIMEDDOOR_H_
 
+#include <stdexcept>
+#include <thread>
+
 class DoorTimerAdapter;
 class Timer;
 class Door;
 class TimedDoor;
 
+class DoorTimeoutException : public std::runtime_error {
+public:
+  explicit DoorTimeoutException(const char *msg) : std::runtime_error(msg) {}
+};
+
 class TimerClient {
- public:
+public:
   virtual void Timeout() = 0;
-};
 
-class Door {
- public:
-  virtual void lock() = 0;
-  virtual void unlock() = 0;
-  virtual bool isDoorOpened() = 0;
-};
+  class TimedDoor : public Door {
+  private:
+    DoorTimerAdapter *adapter;
+    Timer *timer;
+    std::thread timerThread;
+    int iTimeout;
+    bool isOpened;
 
-class DoorTimerAdapter : public TimerClient {
- private:
-  TimedDoor& door;
- public:
-  explicit DoorTimerAdapter(TimedDoor&);
-  void Timeout();
-};
+  public:
+    explicit TimedDoor(int);
+    ~TimedDoor();
+    bool isDoorOpened() override;
+    void unlock() override;
+    void lock() override;
+    int getTimeOut() const;
+    virtual void throwState();
+    Timer *getTimer() const { return timer; }
+    void triggerTimeoutForTest(); // For testing: simulates timer callback
+    void registerTimerForTest(int timeout, TimerClient *client); // For testing
+  };
 
-class TimedDoor : public Door {
- private:
-  DoorTimerAdapter * adapter;
-  int iTimeout;
-  bool isOpened;
- public:
-  explicit TimedDoor(int);
-  bool isDoorOpened();
-  void unlock();
-  void lock();
-  int  getTimeOut() const;
-  void throwState();
-};
+  class Timer {
+    TimerClient *client;
+    void sleep(int);
 
-class Timer {
-  TimerClient *client;
-  void sleep(int);
- public:
-  void tregister(int, TimerClient*);
-};
+  public:
+    void tregister(int, TimerClient *);
+  };
 
-#endif  // INCLUDE_TIMEDDOOR_H_
+#endif // INCLUDE_TIMEDDOOR_H_
