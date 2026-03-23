2a98e03f808aabfaf590ebb67a5552c5fe006365
Induce <aa5619721@gmail.com>
2026-03-23 12:11:23 +0300

    iter 2

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -19,34 +19,51 @@ public:
 class TimerClient {
 public:
   virtual void Timeout() = 0;
+};
 
-  class TimedDoor : public Door {
-  private:
-    DoorTimerAdapter *adapter;
-    Timer *timer;
-    std::thread timerThread;
-    int iTimeout;
-    bool isOpened;
+class Door {
+public:
+  virtual void lock() = 0;
+  virtual void unlock() = 0;
+  virtual bool isDoorOpened() = 0;
+};
 
-  public:
-    explicit TimedDoor(int);
-    ~TimedDoor();
-    bool isDoorOpened() override;
-    void unlock() override;
-    void lock() override;
-    int getTimeOut() const;
-    virtual void throwState();
-    Timer *getTimer() const { return timer; }
-    void triggerTimeoutForTest(); // For testing: simulates timer callback
-    void registerTimerForTest(int timeout, TimerClient *client); // For testing
-  };
+class DoorTimerAdapter : public TimerClient {
+private:
+  TimedDoor &door;
 
-  class Timer {
-    TimerClient *client;
-    void sleep(int);
+public:
+  explicit DoorTimerAdapter(TimedDoor &);
+  void Timeout();
+};
 
-  public:
-    void tregister(int, TimerClient *);
-  };
+class TimedDoor : public Door {
+private:
+  DoorTimerAdapter *adapter;
+  Timer *timer;
+  std::thread timerThread;
+  int iTimeout;
+  bool isOpened;
+
+public:
+  explicit TimedDoor(int);
+  ~TimedDoor();
+  bool isDoorOpened() override;
+  void unlock() override;
+  void lock() override;
+  int getTimeOut() const;
+  virtual void throwState();
+  Timer *getTimer() const { return timer; }
+  void triggerTimeoutForTest();
+  void registerTimerForTest(int timeout, TimerClient *client);
+};
+
+class Timer {
+  TimerClient *client;
+  void sleep(int);
+
+public:
+  void tregister(int, TimerClient *);
+};
 
 #endif // INCLUDE_TIMEDDOOR_H_
